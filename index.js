353683e3a42dbab0896046e15b36e85362277986
Nat <wmainvielle@gmail.com>
2023-03-18 14:05:04 +0100

    chore: add interaction autoReply feature example

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const { Sucrose } = require('discord-sucrose');
 const { GatewayIntentBits, Partials } = require('discord.js');
+const InteractionAutoReplyContent = require('./contents/InteractionAutoReplyContent');
 require('dotenv').config();
 
 Sucrose.build({
   intents: [GatewayIntentBits.Guilds],
   partials: [Partials.Channel],
+
+  // # Optional options
+  features: { // Custom bot features
+    interactions: { // Interactions manager
+      autoReply: { // Interaction manager autoReply feature
+        active: true, // true per default
+
+        // default: https://github.com/Natto-PKP/discord-sucrose/blob/main/src/contents/InteractionAutoReplyContents.ts
+        contents: InteractionAutoReplyContent,
+      },
+    },
+  },
 });
